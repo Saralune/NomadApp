138979c72f39193c101f0638f1ef313b1746ba8a
saralune <sarahlef59@hotmail.fr>
2024-12-01 16:28:43 +0100

    Initial Commit

---
+++ NomadApp/NomadAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NomadAppApp.swift
+//  NomadApp
+//
+//  Created by Sara Lefort on 01/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct NomadAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
